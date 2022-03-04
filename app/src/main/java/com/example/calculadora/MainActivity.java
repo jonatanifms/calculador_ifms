dfbc581207fa6eaaf31f4d448650cb863124ed0f
jonatanifms <jonatan.abade@estudante.ifms.edu.br>
2022-03-03 21:33:54 -0400

    feat: aula de mudança de tela

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,109 +2,115 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText e1, e2;
     TextView t1;
+    Button sum, div, mul, sub;
     double num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sum = findViewById(R.id.sum);
+        sub = findViewById(R.id.sub);
+        mul = findViewById(R.id.mul);
+        div = findViewById(R.id.div);
+
+        sum.setOnClickListener(this);
+        sub.setOnClickListener(this);
+        mul.setOnClickListener(this);
+        div.setOnClickListener(this);
     }
 
-    // a public method to get the input numbers
     public boolean getNumbers() {
-
         e1 = (EditText) findViewById(R.id.num1);
-
         e2 = (EditText) findViewById(R.id.num2);
-
         t1 = (TextView) findViewById(R.id.result);
 
         String s1 = e1.getText().toString();
-
         String s2 = e2.getText().toString();
 
-        // condition to check if box is not empty
         if ((s1.equals(null) && s2.equals(null)) || (s1.equals("") && s2.equals(""))) {
-
-            String result = "Por favor insira um valor!";
-            t1.setText(result);
+            Toast.makeText(this, "Por favor insira um valor!", Toast.LENGTH_SHORT).show();
 
             return false;
         } else {
             num1 = Double.parseDouble(s1);
-
             num2 = Double.parseDouble(s2);
         }
 
         return true;
     }
 
-    // a public method to perform addition
     public void doSum(View v) {
-
-        // get the input numbers
         if (getNumbers()) {
             double sum = num1 + num2;
-            t1.setText(Double.toString(sum));
+            t1.setText(String.format("%.2f", sum));
         }
     }
 
-    // a public method to perform power function
-    public void doPow(View v) {
-
-        // get the input numbers
-        if (getNumbers()) {
-            double sum = Math.pow(num1, num2);
-            t1.setText(Double.toString(sum));
-        }
-    }
-
-    // a public method to perform subtraction
     public void doSub(View v) {
-
-        // get the input numbers
         if (getNumbers()) {
             double sum = num1 - num2;
-            t1.setText(Double.toString(sum));
+            t1.setText(String.format("%.2f", sum));
         }
     }
 
     public void doMul(View v) {
         if (getNumbers()) {
             double sum = num1 * num2;
-            t1.setText(Double.toString(sum));
+            t1.setText(String.format("%.2f", sum));
         }
     }
 
     public void doDiv(View v) {
-
-        // get the input numbers
         if (getNumbers()) {
-
-            // displaying the text in text view assigned as t1
-            double sum = num1 / (num2 * 1.0);
-            t1.setText(Double.toString(sum));
+            if (num1 == 0 || num2 == 0) {
+                Toast.makeText(this, "Não é possivel fazer divisão por 0!", Toast.LENGTH_SHORT).show();
+            } else {
+                double sum = num1 / (num2 * 1.0);
+                t1.setText(String.format("%.2f", sum));
+            }
         }
     }
 
-    // a public method to perform modulus function
-    public void doMod(View v) {
+    public void clearAll(View v) {
+        e1.setText("");
+        e2.setText("");
+        t1.setText("");
+    }
 
-        // get the input numbers
-        if (getNumbers()) {
-            double sum = num1 % num2;
-            t1.setText(Double.toString(sum));
+    public void changeScreen(View v) {
+        Intent intent = new Intent(this, OutherActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sum:
+                this.doSum(v);
+                break;
+            case R.id.sub:
+                this.doSub(v);
+                break;
+            case R.id.mul:
+                this.doMul(v);
+                break;
+            case R.id.div:
+                this.doDiv(v);
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/example/calculadora/OutherActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculadora;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OutherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.outher_activity);
+
+    }
+
+}
